03eefef4c9da9ea4d19f1422fbaa01073692c7e5
Hassan <hrana988@gmail.com>
2025-08-19 16:08:45 +0500

    optimised speed and loading times for character pages - suspense boundary

--- app/characters/page.tsx
+++ app/characters/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import CharacterGrid from "@/components/CharacterGrid";
+import {Suspense} from "react";
 
 export default function CharactersPage() {
     return (
         <div className="p-4">
             <h1 className="text-2xl font-semibold mb-4">Characters</h1>
-            <CharacterGrid />
+            <Suspense>
+                <CharacterGrid />
+            </Suspense>
         </div>
     );
 }
